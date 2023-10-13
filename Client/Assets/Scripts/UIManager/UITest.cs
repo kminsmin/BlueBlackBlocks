f4eff616353f4b593333f531de4b852dd830fe27
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-10-13 14:23:13 +0900

    [UI] 매니저 생성 및 옵션 팝업 구현

---
+++ Client/Assets/Scripts/UIManager/UITest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log(UIManager.Instance);
+    }
+
+}
