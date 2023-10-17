64e92a4b8018331b70bc9c1cd5267fc090c6cf18
yarogono <70641418+yarogono@users.noreply.github.com>
2023-10-17 11:06:46 +0900

    Feature/#27 WebManager

---
+++ Client/Assets/Scripts/Managers/WebManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebManager : CustomSingleton<WebManager>
+{
+    public string BaseUrl { get; set; } = "https://localhost:7074/";
+
+    public void SendPostRequest<T>(string url, object obj, Action<T> res)
+    {
+        StartCoroutine(CoSendWebRequest(url, UnityWebRequest.kHttpVerbPOST, obj, res));
+    }
+
+    IEnumerator CoSendWebRequest<T>(string url, string method, object obj, Action<T> res)
+    {
+        string sendUrl = $"{BaseUrl}/{url}";
+
+        byte[] jsonBytes = null;
+        if (obj != null)
+        {
+            string jsonStr = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            jsonBytes = Encoding.UTF8.GetBytes(jsonStr);
+        }
+
+        using (var uwr = new UnityWebRequest(sendUrl, method))
+        {
+            uwr.uploadHandler = new UploadHandlerRaw(jsonBytes);
+            uwr.downloadHandler = new DownloadHandlerBuffer();
+            uwr.SetRequestHeader("Content-Type", "application/json");
+
+            yield return uwr.SendWebRequest();
+
+            if (uwr.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                T resObj = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(uwr.downloadHandler.text);
+                res.Invoke(resObj);
+            }
+        }
+    }
+}
