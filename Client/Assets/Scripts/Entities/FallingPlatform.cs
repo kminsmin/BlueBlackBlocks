bd3f225fb33379fb0b51289898291dfc585e87d5
이경민(Kyung Min Lee) <kminsmin@gmail.com>
2023-10-17 22:23:22 +0900

    Block Color feature

---
+++ Client/Assets/Scripts/Entities/FallingPlatform.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private Transform _transform;
+    private Vector3 _fall = Vector3.zero;
+    private Vector3 _initialPos = Vector3.zero;
+    private bool _isColliding;
+    [SerializeField] private float _fallSpeed = 1.5f;
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+    private void Start()
+    {
+        _fall = new Vector3(0, -1, 0) * _fallSpeed * Time.deltaTime;
+        _initialPos = _transform.position;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision != null)
+        {
+            if(collision.gameObject.CompareTag("Blue")||collision.gameObject.CompareTag("Black"))
+            {
+                _isColliding = true;
+                _transform.Translate(_fall);
+            }
+         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (_transform.position.y < _initialPos.y)
+            {
+                _isColliding = false;
+            }
+            if (!_isColliding)
+            {
+                _transform.DOLocalMoveY(_initialPos.y, 1f);
+            }
+
+        }
+    }
+}
--- Client/Assets/Scripts/Managers/StageManager.cs
+++ Client/Assets/Scripts/Managers/StageManager.cs
@@ -1,23 +1,108 @@
+using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class StageManager : MonoBehaviour
+public class StageManager : MonoBehaviourPunCallbacks
 {
     public static StageManager Instance;
+    public event Action OnBlueDeath;
+    public event Action OnBlackDeath;
+    public int CurrentCheckPointIndex { get; private set; } = 0;
+    public UIItem UIItem { get; private set; }
+    private GameObject _bluePlayer;
+    private GameObject _blackPlayer;
+    private Rigidbody2D _playerRigidBody;
+    [SerializeField] private TilemapCollider2D[] _terrainColliders;
+    [SerializeField] private Transform[] _checkPoints;
+    [SerializeField] private float _respawnInterval = 3f; 
+    [SerializeField] GameObject _cinemachine;
 
     private void Awake()
     {
         Instance = this;
+        //TODO: photonView 추가한 플레이어 오브젝트 프리팹화, Resources 폴더에 넣어서 Load 후 Instantiate
+        /*
+         * if(photonView.AmOwner) 아이템 생성 메서드 실행(상황봐서 Start로 옮길 예정)
+         */
+        _bluePlayer = GameObject.FindGameObjectWithTag("Blue");
+        _blackPlayer = GameObject.FindGameObjectWithTag("Black");
+        
+        UIItem = UIManager.Instance.OpenUI<UIItem>();
     }
     void Start()
     {
-        
+        _playerRigidBody = _bluePlayer.GetComponent<Rigidbody2D>();
+        _bluePlayer.SetActive(false);
+        _blackPlayer.SetActive(false);
+        Invoke("StartGame", 60f);
+        OnBlueDeath += InvokeRespawnBlue;
+        OnBlackDeath += InvokeRespawnBlack;
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CheckJump(WaitForSeconds checkInterval)
     {
-        
+        while(true)
+        {
+            if (_playerRigidBody != null)
+            {
+                if (_playerRigidBody.velocity.y > 0)
+                {
+                    foreach(var terrain in _terrainColliders)
+                    {
+                        terrain.enabled = false;
+                    }
+                }
+                else
+                {
+                    foreach(var terrain in _terrainColliders)
+                    { terrain.enabled = true; }
+                }
+            }
+            yield return checkInterval;
+
+        } 
+    }
+
+    private void StartGame()
+    {
+        _bluePlayer.SetActive(true);
+        _blackPlayer.SetActive(true);
+        _cinemachine.SetActive(false);
+        StartCoroutine(CheckJump(new WaitForSeconds(.05f)));
+    }
+
+    public void CallBlueDeathEvent()
+    {
+        _bluePlayer.SetActive(false);
+        OnBlueDeath?.Invoke();
+    }
+
+    public void CallBlackDeathEvent()
+    {
+        _blackPlayer.SetActive(false);
+        OnBlackDeath?.Invoke();
+    }
+
+    private void InvokeRespawnBlue()
+    {
+        Invoke("RespawnBlue", _respawnInterval);
+    }
+    private void RespawnBlue()
+    {
+        _bluePlayer.transform.position = _checkPoints[CurrentCheckPointIndex].position;
+        _bluePlayer.gameObject.SetActive(true);
+    }
+
+    private void InvokeRespawnBlack()
+    {
+        Invoke("RespawnBlack", _respawnInterval);
+    }
+    private void RespawnBlack()
+    {
+        _blackPlayer.transform.position = _checkPoints[CurrentCheckPointIndex].position;
+        _blackPlayer.gameObject.SetActive(true);
     }
 }
